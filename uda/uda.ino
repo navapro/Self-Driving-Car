3fd7bfbdb29bbddbc84b423a734cfe2c3a503a0f
Naveen <navaneethk002@gmail.com>
2022-07-10 11:22:05 -0500

    Update uda.ino
    Added comments and removed some redundencies.

--- uda/uda.ino
+++ uda/uda.ino
@@ -1,294 +1,202 @@
-#include <IRremote.hpp>
-    
 const int EnableL = 5;
-const int HighL = 6;       // LEFT SIDE MOTOR
-const int LowL =7;
+const int HighL = 6; // LEFT SIDE MOTOR
+const int LowL = 7;
 
 const int EnableR = 10;
-const int HighR = 8;       //RIGHT SIDE MOTOR
-const int LowR =9;
+const int HighR = 8; // RIGHT SIDE MOTOR
+const int LowR = 9;
 
-const int D0 = 11;       //Raspberry pin 21    LSB
-const int D1 = 12;       //Raspberry pin 22
-const int D2 = 13;       //Raspberry pin 23  
+const int D0 = 11; // Raspberry pin 21    LSB
+const int D1 = 12; // Raspberry pin 22
+const int D2 = 13; // Raspberry pin 23
 
-bool state = false;
-const long REMOTE_KEY_INT = 3125149440;
-
-int a,b,c,d,data;
-
-
-void setup() {
-
-IrReceiver.begin(4, ENABLE_LED_FEEDBACK);
-
-pinMode(EnableL, OUTPUT);
-pinMode(HighL, OUTPUT);
-pinMode(LowL, OUTPUT);
-
-
-pinMode(EnableR, OUTPUT);
-pinMode(HighR, OUTPUT);
-pinMode(LowR, OUTPUT);
-
-pinMode(D0, INPUT_PULLUP);
-pinMode(D1, INPUT_PULLUP);
-pinMode(D2, INPUT_PULLUP);
-
-Serial.begin(9600);
-}
-
-bool ir_remote_function() {
-  if (IrReceiver.decode()) {
-      IrReceiver.resume(); // Enable receiving of the next value
-      if (IrReceiver.decodedIRData.decodedRawData == REMOTE_KEY_INT){
-        return true;  
-      }
-  } else {
-    return false;
-    }
-}
-
-void Data()
+void setup()
 {
-   a = digitalRead(D0);
-   b = digitalRead(D1);
-   c = digitalRead(D2);
-   data = 4*c+2*b+a;
+
+    // Set pinModes
+    pinMode(EnableL, OUTPUT);
+    pinMode(HighL, OUTPUT);
+    pinMode(LowL, OUTPUT);
+
+    pinMode(EnableR, OUTPUT);
+    pinMode(HighR, OUTPUT);
+    pinMode(LowR, OUTPUT);
+
+    pinMode(D0, INPUT_PULLUP);
+    pinMode(D1, INPUT_PULLUP);
+    pinMode(D2, INPUT_PULLUP);
+}
+
+int Data()
+{
+    int a = digitalRead(D0);
+    int b = digitalRead(D1);
+    int c = digitalRead(D2);
+    int data = 4 * c + 2 * b + a;
+    return data;
 }
 
 void Forward()
 {
-  digitalWrite(HighL, LOW);
-  digitalWrite(LowL, HIGH);
-  analogWrite(EnableL,255);
+    digitalWrite(HighL, LOW);
+    digitalWrite(LowL, HIGH);
+    analogWrite(EnableL, 255);
 
-  digitalWrite(HighR, LOW);
-  digitalWrite(LowR, HIGH);
-  analogWrite(EnableR,255);
-  
+    digitalWrite(HighR, LOW);
+    digitalWrite(LowR, HIGH);
+    analogWrite(EnableR, 255);
 }
-
 
 void Backward()
 {
-  digitalWrite(HighL, HIGH);
-  digitalWrite(LowL, LOW);
-  analogWrite(EnableL,255);
+    digitalWrite(HighL, HIGH);
+    digitalWrite(LowL, LOW);
+    analogWrite(EnableL, 255);
 
-  digitalWrite(HighR, HIGH);
-  digitalWrite(LowR, LOW);
-  analogWrite(EnableR,255);
-  
+    digitalWrite(HighR, HIGH);
+    digitalWrite(LowR, LOW);
+    analogWrite(EnableR, 255);
 }
 
 void Stop()
 {
-  digitalWrite(HighL, LOW);
-  digitalWrite(LowL, HIGH);
-  analogWrite(EnableL,0);
+    digitalWrite(HighL, LOW);
+    digitalWrite(LowL, HIGH);
+    analogWrite(EnableL, 0);
 
-  digitalWrite(HighR, LOW);
-  digitalWrite(LowR, HIGH);
-  analogWrite(EnableR,0);
-  
+    digitalWrite(HighR, LOW);
+    digitalWrite(LowR, HIGH);
+    analogWrite(EnableR, 0);
 }
 
-void Left1()
+void Left(int left_motor, int right_motor)
 {
-  digitalWrite(HighL, LOW);
-  digitalWrite(LowL, HIGH);
-  analogWrite(EnableL,100);
+    digitalWrite(HighL, LOW);
+    digitalWrite(LowL, HIGH);
+    analogWrite(EnableL, left_motor);
 
-  digitalWrite(HighR, LOW);
-  digitalWrite(LowR, HIGH);
-  analogWrite(EnableR,255);
-  
+    digitalWrite(HighR, LOW);
+    digitalWrite(LowR, HIGH);
+    analogWrite(EnableR, right_motor);
 }
 
-void Left2()
+void Right(int left_motor, int right_motor)
 {
-  digitalWrite(HighL, LOW);
-  digitalWrite(LowL, HIGH);
-  analogWrite(EnableL,50);
+    digitalWrite(HighL, LOW);
+    digitalWrite(LowL, HIGH);
+    analogWrite(EnableL, left_motor);
 
-  digitalWrite(HighR, LOW);
-  digitalWrite(LowR, HIGH);
-  analogWrite(EnableR,255);
-  
-}
-
-
-void Left3()
-{
-  digitalWrite(HighL, LOW);
-  digitalWrite(LowL, HIGH);
-  analogWrite(EnableL,0);
-
-  digitalWrite(HighR, LOW);
-  digitalWrite(LowR, HIGH);
-  analogWrite(EnableR,255);
-  
-}
-
-void Right1()
-{
-  digitalWrite(HighL, LOW);
-  digitalWrite(LowL, HIGH);
-  analogWrite(EnableL,255);
-
-  digitalWrite(HighR, LOW);
-  digitalWrite(LowR, HIGH);
-  analogWrite(EnableR,100);  //200
-  
-}
-void Right2()
-{
-  digitalWrite(HighL, LOW);
-  digitalWrite(LowL, HIGH);
-  analogWrite(EnableL,255);
-
-  digitalWrite(HighR, LOW);
-  digitalWrite(LowR, HIGH);
-  analogWrite(EnableR,50);   //160
-  
-}
-
-void Right3()
-{
-  digitalWrite(HighL, LOW);
-  digitalWrite(LowL, HIGH);
-  analogWrite(EnableL,255);
-
-  digitalWrite(HighR, LOW);
-  digitalWrite(LowR, HIGH);
-  analogWrite(EnableR,0);   //100
-  
+    digitalWrite(HighR, LOW);
+    digitalWrite(LowR, HIGH);
+    analogWrite(EnableR, right_motor);
 }
 
 void UTurn()
 {
-  analogWrite(EnableL, 0);
-  analogWrite(EnableR, 0);
-  delay(400);
+    analogWrite(EnableL, 0);
+    analogWrite(EnableR, 0);
+    delay(400);
 
-  analogWrite(EnableL, 250);
-  analogWrite(EnableR, 250);    //forward
-  delay(1000);
+    analogWrite(EnableL, 250);
+    analogWrite(EnableR, 250); // forward
+    delay(1000);
 
-  analogWrite(EnableL, 0);
-  analogWrite(EnableR, 0);
-  delay(400);
+    analogWrite(EnableL, 0);
+    analogWrite(EnableR, 0);
+    delay(400);
 
-  digitalWrite(HighL, HIGH);
-  digitalWrite(LowL, LOW);
-  digitalWrite(HighR, LOW);   //   left
-  digitalWrite(LowR, HIGH);
-  analogWrite(EnableL, 255);
-  analogWrite(EnableR, 255);
-  delay(700);
+    digitalWrite(HighL, HIGH);
+    digitalWrite(LowL, LOW);
+    digitalWrite(HighR, LOW); //   left
+    digitalWrite(LowR, HIGH);
+    analogWrite(EnableL, 255);
+    analogWrite(EnableR, 255);
+    delay(700);
 
-  analogWrite(EnableL, 0);
-  analogWrite(EnableR, 0);
-  delay(400);
+    analogWrite(EnableL, 0);
+    analogWrite(EnableR, 0);
+    delay(400);
 
-  digitalWrite(HighL, LOW);
-  digitalWrite(LowL, HIGH);
-  digitalWrite(HighR, LOW);   // forward
-  digitalWrite(LowR, HIGH);
-  analogWrite(EnableL, 255);
-  analogWrite(EnableR, 255);
-  delay(900);
+    digitalWrite(HighL, LOW);
+    digitalWrite(LowL, HIGH);
+    digitalWrite(HighR, LOW); // forward
+    digitalWrite(LowR, HIGH);
+    analogWrite(EnableL, 255);
+    analogWrite(EnableR, 255);
+    delay(900);
 
-  analogWrite(EnableL, 0);
-  analogWrite(EnableR, 0);
-  delay(400);
+    analogWrite(EnableL, 0);
+    analogWrite(EnableR, 0);
+    delay(400);
 
-  digitalWrite(HighL, HIGH);
-  digitalWrite(LowL, LOW);
-  digitalWrite(HighR, LOW);    //left
-  digitalWrite(LowR, HIGH);
-  analogWrite(EnableL, 255);
-  analogWrite(EnableR, 255);
-  delay(700);
+    digitalWrite(HighL, HIGH);
+    digitalWrite(LowL, LOW);
+    digitalWrite(HighR, LOW); // left
+    digitalWrite(LowR, HIGH);
+    analogWrite(EnableL, 255);
+    analogWrite(EnableR, 255);
+    delay(700);
 
+    analogWrite(EnableL, 0);
+    analogWrite(EnableR, 0);
+    delay(1000);
 
-  analogWrite(EnableL, 0);
-  analogWrite(EnableR, 0);
-  delay(1000);
-
-
-
-  digitalWrite(HighL, LOW);
-  digitalWrite(LowL, HIGH);
-  digitalWrite(HighR, LOW);
-  digitalWrite(LowL, HIGH);
-  analogWrite(EnableL, 150);
-  analogWrite(EnableR, 150);
-  delay(300);
+    digitalWrite(HighL, LOW);
+    digitalWrite(LowL, HIGH);
+    digitalWrite(HighR, LOW);
+    digitalWrite(LowL, HIGH);
+    analogWrite(EnableL, 150);
+    analogWrite(EnableR, 150);
+    delay(300);
 }
 
-
-
-void loop() 
+void loop()
 {
+    // Access the raspberrypi input.
+    int data = Data();
 
-  Data();
-
-  bool ir_remote = ir_remote_function();
-  if (ir_remote && state == false){
-    state = true;
-  } else if (ir_remote && state == true){
-      state = false;
+    // Setting variables array for intensity of turns.
+    if (data == 0)
+    {
+        Forward();
+    }
+    else if (data == 1)
+    {
+        Right(255, 100);
     }
 
-if (state == true){
-  Serial.println(data);
-  if(data==0)
-   {
-     Forward();
-   }
-   
-  else if(data==1)
-   {
-     Right1();
-   }
-     
-  else if(data==2)
-   {
-     Right2();
-   }
-     
-  else if(data==3)
-   {
-     Right3();
-   }
-     
-  else if(data==4)
-   {
-     Left1();
-   }
-    
-  else if(data==5)
-   {
-     Left2();
-   }
-    
-  else if(data==6)
-   {
-     Left3();
-   }
-  
-  else if (data==7)
-   {
-     UTurn();
-   }
-    
-  else if (data>7)
-   {
-     Stop();
-   }
-   
+    else if (data == 2)
+    {
+        Right(255, 50);
+    }
 
-}
+    else if (data == 3)
+    {
+        Right(255, 0);
+    }
+
+    else if (data == 4)
+    {
+        Left(100, 255);
+    }
+
+    else if (data == 5)
+    {
+        Left(50, 255);
+    }
+
+    else if (data == 6)
+    {
+        Left(0, 255);
+    }
+
+    else if (data == 7)
+    {
+        UTurn();
+    }
+
+    else if (data > 7)
+    {
+        Stop();
+    }
 }
